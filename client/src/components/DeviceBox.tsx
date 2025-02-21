7b74ca76bcc9cf16423ab7e44b3459a2511ed978
Callum Forsyth <c-forsyth02@sky.com>
2025-02-21 09:48:49 +0000

    Allows the image and the text to change based on inputed devicename

--- client/src/components/DeviceBox.tsx
+++ client/src/components/DeviceBox.tsx
@@ -1,26 +1,60 @@
 import React, { useState } from 'react';
 import roombaImage from '../assets/roomba.svg';
+import lightSwitchImage from '../assets/light_switch.svg';
+import outletImage from '../assets/outlet.svg';
+import '../App.css';
 
 export default function DeviceBox() {
     const [isSliderOn, setIsSliderOn] = useState(false); // State for slider
     const [text, setText] = useState("Roomba"); // State for text under the image
+    const [image, setImage] = useState(roombaImage); // State for image
 
     const toggleSlider = () => {
         setIsSliderOn(!isSliderOn);
     };
 
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const deviceName = e.target.value;
+        setText(deviceName);
+
+        // Change image based on input value
+        switch (deviceName.toLowerCase()) {
+            case 'roomba':
+                setImage(roombaImage);
+                break;
+            case 'light switch':
+                setImage(lightSwitchImage);
+                break;
+            case 'outlet':
+                setImage(outletImage);
+                break;
+            default:
+                setImage(roombaImage);
+                break;
+        }
+    };
+
     return (
-        <div className="device-box">
+        <div className={`device-box ${isSliderOn ? 'device-box-on' : 'device-box-off'}`}>
             {/* Image */}
             <img
-                src={roombaImage}
-                alt="Roomba"
+                src={image}
+                alt={text}
             />
 
             {/* Text Under the Image */}
             <p>
                 {text}
             </p>
+
+            {/* Input Field to Change Text and Image */}
+            <input
+                type="text"
+                value={text}
+                onChange={handleInputChange}
+                className="device-input"
+                placeholder="Enter device name"
+            />
 
             {/* Slider Switch in Bottom-Left Corner */}
             <div className="slider-container">
