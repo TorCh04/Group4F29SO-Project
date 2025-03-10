ed4bc259fadfe4e702f2ae05b6e2a6b24deeb411
Vbalimidi <vb2023@hw.ac.uk>
2025-03-10 20:05:46 +0000

    energy tracker

---
+++ server/models/energy.js
@@ -0,0 +1,30 @@
+// EnergyUsage schema and model
+const energyUsageSchema = new mongoose.Schema({
+  device: { type: String, required: true },
+  data: [
+    {
+      timestamp: { type: Date, default: Date.now },
+      energyUsage: { type: Number, required: true },
+    },
+  ],
+});
+
+const EnergyUsage = mongoose.model("EnergyUsage", energyUsageSchema);
+
+exports.EnergyUsage = EnergyUsage;
+
+//EnergyGenerated schema and model
+// const energyGeneratedSchema = new mongoose.Schema({
+//   device: { type: String, required: true },
+//   data: [
+//     {
+//       timestamp: { type: Date, default: Date.now },
+//       energyGenerated: { type: Number, required: true },
+//     },
+//   ],
+// });
+
+// const EnergyGenerated = mongoose.model(
+//   "EnergyGenerated",
+//   energyGeneratedSchema
+// );
