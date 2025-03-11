c321cfeb5aa946d6802c07ca388b3e6e0cd74b81
Callum Forsyth <c-forsyth02@sky.com>
2025-03-11 11:22:38 +0000

    functional sidebar shrink on smaller screens

--- client/src/components/Dashboard/Sidebar.tsx
+++ client/src/components/Dashboard/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo_vertical.svg';
 import useSimulationData from './hooks/useSimulationData';
@@ -5,28 +6,38 @@ import useSimulationData from './hooks/useSimulationData';
 export default function Sidebar() {
   const navigate = useNavigate();
   const { humidity, temperature } = useSimulationData();
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   function logout() {
     localStorage.removeItem('token');
     navigate('/');
   }
 
+  function toggleMenu() {
+    setMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <div className="sidebar__container">
-      <Link to="/">
-        <img src={logo} alt="Logo" />
-      </Link>
-      <div className="sidebar__links">
-        <Link to="/devices" className="sidebar__item">Smart Devices</Link>
-        <Link to="/energy" className="sidebar__item">Energy Tracker</Link>
-        <Link to="/leaderboard" className="sidebar__item">Leaderboard</Link>
-        <Link to="/settings" className="sidebar__settings">Settings</Link>
+    <div>
+      <button className="menu-button" onClick={toggleMenu}>
+        ☰
+      </button>
+      <div className={`sidebar__container ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <div className="sidebar__links">
+          <Link to="/devices" className="sidebar__item">Smart Devices</Link>
+          <Link to="/energy" className="sidebar__item">Energy Tracker</Link>
+          <Link to="/leaderboard" className="sidebar__item">Leaderboard</Link>
+          <Link to="/settings" className="sidebar__settings">Settings</Link>
+        </div>
+        <div className="sidebar__simulation-data">
+          <p>🌡️ {temperature !== null ? temperature : 'Loading...'}°C</p>
+          <p>💧 {humidity !== null ? humidity : 'Loading...'}%</p>
+        </div>
+        <button onClick={logout}>Logout</button>
       </div>
-      <div className="sidebar__simulation-data">
-      <p>🌡️ {temperature !== null ? temperature : 'Loading...'}°C</p>
-        <p>💧 {humidity !== null ? humidity : 'Loading...'}%</p>
-      </div>
-      <button onClick={logout}>Logout</button>
     </div>
   );
 }
